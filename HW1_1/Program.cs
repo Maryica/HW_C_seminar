a658f957ba93d992fceecb1bffc2d2d06ca9ceea
Maria <pavla0806@gmail.com>
2023-06-14 22:58:46 +0300

    a program that takes two numbers as input and outputs which number is larger and which is smaller

--- HW1_1/Program.cs
+++ HW1_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int number_1 = int.Parse(Console.ReadLine()!);
+int number_2 = int.Parse(Console.ReadLine()!);
+if (number_1 > number_2)
+{
+    Console.WriteLine("max = " + number_1 + ", min = " + number_2);
+}
+
+else
+{
+    Console.WriteLine("max = " + number_2 + ", min = " + number_1);
+}
