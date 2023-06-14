58dc8f0dbc1319f266cc646d25a23f2c33485227
Maria <pavla0806@gmail.com>
2023-06-14 23:12:15 +0300

    a program that takes a number as input and outputs whether the number is even, which takes a number (N) as input, and shows all even numbers from 1 to N as output

---
+++ HW1_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int number = int.Parse(Console.ReadLine()!);
+int count = 0;
+if (number % 2 == 0)
+{
+    while (count < number)
+    {
+        count = count + 2;
+        Console.WriteLine(count);
+    }
+
+}
+else
+{
+    while (count < (number - 1))
+    {
+        count = count + 2;
+        Console.WriteLine(count);
+    }
+
+}
