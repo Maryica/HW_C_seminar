222ccafcebf8cd53af5ad1359559c40cf5e9ec03
Maria <pavla0806@gmail.com>
2023-06-14 23:07:59 +0300

    a program that accepts three numbers as input and outputs the maximum of these numbers

---
+++ HW1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int number_1 = int.Parse(Console.ReadLine()!);
+int number_2 = int.Parse(Console.ReadLine()!);
+int number_3 = int.Parse(Console.ReadLine()!);
+
+if (number_1 > number_2 && number_1 > number_3)
+{
+    Console.WriteLine(number_1);
+}
+else if (number_2 > number_1 && number_2 > number_3)
+{
+    Console.WriteLine(number_2);
+}
+else
+{
+    Console.WriteLine(number_3);
+}
