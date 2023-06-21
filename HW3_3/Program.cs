9253af1b0047083e5dba149503c96307d89f4b57
Maria <pavla0806@gmail.com>
2023-06-21 23:43:15 +0300

    a program that accepts a number (N) as input and outputs a table of cubes of numbers from 1 to N

---
+++ HW3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+int number = Prompt("Введите число: ");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i * i * i}");
+}
